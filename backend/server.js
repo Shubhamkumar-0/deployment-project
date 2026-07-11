34745bcc2822f65b004a90e63d9727eb688b7bd4
Shubham Kumar <shubhamkumarerror1@gmail.com>
2026-07-11 14:10:34 +0530

    fix cros

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,38 @@
 
 
 // updated codeeeeeeeeeeeeeeeeeeeeeeeeee
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+
+// const connectDB = require("./config/database");
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is Running 🚀");
+// });
+
+// app.get("/api/test", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Backend Connected Successfully",
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -30,7 +62,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// 👇 Replace app.use(cors()) with this
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
