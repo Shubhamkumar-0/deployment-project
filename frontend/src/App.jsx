08775cab21c0c57d8124a891d13825ff1d40b5fe
Shubham Kumar <shubhamkumarerror1@gmail.com>
2026-07-16 00:24:23 +0530

    register page code add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import Register from "./pages/Register";
+import Register from "./pages/Register.jsx";
 // import "./App.css";
 
 function App() {
