f79c286a1345a3f96b28970d273ea0e4c51c7a95
Shubham Kumar <shubhamkumarerror1@gmail.com>
2026-07-16 19:55:22 +0530

    Added login api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,5 +43,51 @@ const register = async (req, res) => {
   }
 };
 
+
+// ===============================
+// Login User
+// ===============================
+
+const login = async (req, res) => {
+  try {
+
+    // Get email & password
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    // User not found
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Check password
+    if (user.password !== password) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    // Login Success
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      user,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+};
 // Export Controller
-module.exports = { register };
+module.exports = { register,login, };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 // Import Controller
-const { register } = require("../controllers/authController");
+const { register,login } = require("../controllers/authController");
 
 // Register API
 // POST /api/auth/register
 router.post("/register", register);
+router.post("/login", login);
 
 // Export Router
 module.exports = router;
