1a00d5b256cb375fdd03cc780c6fc88bbb1f6a35
Shubham Kumar <shubhamkumarerror1@gmail.com>
2026-07-16 20:19:05 +0530

    Added login page and React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,29 +2,17 @@ import { useState } from "react";
 import axios from "axios";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import { Routes, Route } from "react-router-dom";
 // import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
 
-  const getMessage = async () => {
-    try {
-      const res = await axios.get("https://deployment-project-yxly.onrender.com");
-      setMessage(res.data);
-    } catch (error) {
-      console.log(error);
-    } 
-  };
+  
 
   return (
     <div>
       <h1>Deployment Project</h1>
-
-      {/* <button onClick={getMessage}>
-        Connect Backend
-      </button>
-
-      <h2>{message}</h2> */}
 
       {/* this route is for testing and nothing else */}
       {/* <Register />
